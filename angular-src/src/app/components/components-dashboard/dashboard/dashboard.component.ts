9d7b22a862a6ba10369ceac40f333391ce13e55b
slin63 <slin63@illinois.edu>
2017-11-09 19:53:58 -0600

    dashboard UI kinda nice

--- angular-src/src/app/components/components-dashboard/dashboard/dashboard.component.ts
+++ angular-src/src/app/components/components-dashboard/dashboard/dashboard.component.ts
@@ -10,21 +10,42 @@ import { UsergroupService } from '../../../services/usergroup.service';
 export class DashboardComponent implements OnInit {
   usergroups: any[];
 
+  viewGroupControlsButton: boolean;
+  viewGroupControls: boolean;
+  viewGroups: boolean;
+
   constructor(
     private usergroupService: UsergroupService
   ) { }
 
   ngOnInit() {
+    this.initializeGroupMenu();
+  }
+
+  initializeGroupMenu() {
     this.usergroupService.getUserGroups().subscribe(res => {
       this.usergroups = res.user_groups;
-      // console.log(this.usergroups);
+
+      // No groups? Just show the registration tab
+      if (this.usergroups.length === 0) {
+        this.viewGroups = false;
+        this.viewGroupControls = true;
+      } 
+      // We have groups? Show the groups and let the users choose to see the group registration/creation tab
+      else {
+        this.viewGroupControlsButton = true;
+        this.viewGroups = true;
+      }
     },
     err => {
       console.log(err);
       return false;
     });
+  }
 
-
+  onViewGroupControlsClick() {
+    this.viewGroups = !this.viewGroups;
+    this.viewGroupControls = !this.viewGroupControls;
   }
 
 }
