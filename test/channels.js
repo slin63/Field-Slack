36af468ae1d1ca1aab88f983e41d126f307e35c3
slin63 <slin63@illinois.edu>
2017-11-16 20:40:01 -0600

    Turns out I'm stupid. Implemented promises in NODE API tests. I was using the wrong model names that took so long to figure out jesus

--- test/channels.js
+++ test/channels.js
@@ -2,56 +2,55 @@ process.env.NODE_ENV = 'test';
 
 let mongoose = require('mongoose');
 let Channel = require('../models/channel');
+let UserGroup = require('../models/usergroup');
 let User = require('../models/user');
 
+mongoose.Promise = global.Promise;
 
 let chai = require('chai');
+let expect = require('chai').expect;
+let chaiAsPromised = require('chai-as-promised');
 let chaiHttp = require('chai-http');
 let server = require('../app');
 let should = chai.should();
 
 chai.use(chaiHttp);
+chai.use(chaiAsPromised);
 
 // https://stackoverflow.com/questions/37284600/how-to-test-a-node-api-that-uses-jwt-authentication-with-user-login-to-get-toke
 // https://scotch.io/tutorials/test-a-node-restful-api-with-mocha-and-chai
 
-// Before:
-//  Clear usergroups, users, channels
-//  Create user, usergroup
-
-
 
 describe('ChannelTests', () => {
-    var token = '';
+    // Before:
+    //  Declare auth token for later tests
+    //  Create user, usergroup
+
     before((done) => {
+        var token = '';
         testUser = {
             name: 'testName',
             email: 'testEmail@g.edu',
             username: 'testUsername',
             password: 'testPW'
         }
+        
         chai.request(server)
-            .post('/users/register')
+        .post('/users/register')
+        .send(testUser)
+        .end((err, res) => {
+            // console.log('NEXT');
+            expect(res).status.should.equal(200);
+            chai.request(server)
+            .post('/users/authenticate')
             .send(testUser)
             .end( (err, res) => {
+                token = res.body.token;
                 res.status.should.equal(200);
-
-                chai.request(server)
-                    .post('/users/authenticate')
-                    .send(testUser)
-                    .end( (err, res) => {
-                        token = res.body.token;
-                        res.status.should.equal(200);
-                        done();
-                    });
+                done();
             });
-    });
-
-    after((done) => {
-        User.remove({}, (err) => { 
-            done();
         });
-    })
+    });
 
     beforeEach((done) => {
         Channel.remove({}, (err) => { 
@@ -78,7 +77,25 @@ describe('ChannelTests', () => {
                     done();
                 });
         });
-        
+    });
+
+
+    after((done) => {
+        var promise = User.remove({}).exec();
+
+        promise.then(() => {
+            return UserGroup.remove({}).exec();
+        })
+        .then(() => {
+            return Channel.remove({}).exec();
+        })
+        .then(() => {
+            done();
+        })
+        .catch((err) => {
+            throw err;
+            done();
+        });
     });
 });
 
